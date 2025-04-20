a5a82c38762f7b02467cbb5aef0b531cd4b71047
maycon <mayconsrc1117@gmail.com>
2025-04-19 23:17:13 -0300

    Update

---
+++ introdução/hello-word/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello world React</h1>
+      <p>Essse paragrafo é apenas um teste de reação</p>
+      
+    </div>
+  );
+}
+
+export default App;
